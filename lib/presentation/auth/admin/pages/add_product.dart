4d49a9d22af423dd61867080093657b3de41e701
Micleoo <peeka1525@gmail.com>
2024-11-12 01:49:03 +0700

    menambahkan bagian deskripsi produk di add product

--- lib/presentation/auth/admin/pages/add_product.dart
+++ lib/presentation/auth/admin/pages/add_product.dart
@@ -19,6 +19,8 @@ class _AddProductState extends State<AddProduct> {
   final ImagePicker _picker = ImagePicker();
   File? selectedImage;
   TextEditingController namecontroller = TextEditingController();
+  TextEditingController pricecontroller = TextEditingController();
+  TextEditingController detailcontroller = TextEditingController();
 
   Future<void> getImage() async {
     var image = await _picker.pickImage(source: ImageSource.gallery);
@@ -28,29 +30,35 @@ class _AddProductState extends State<AddProduct> {
     }
   }
 
-uploadItem() async {
-  if(selectedImage!=null &&namecontroller.text!=""){
-    String addId= randomAlphaNumeric(10);
-    Reference firebaseStorageRef= FirebaseStorage.instance.ref().child("blogImage").child(addId);
+  uploadItem() async {
+    if (selectedImage != null && namecontroller.text != "") {
+      String addId = randomAlphaNumeric(10);
+      Reference firebaseStorageRef =
+          FirebaseStorage.instance.ref().child("blogImage").child(addId);
 
-    final UploadTask task= firebaseStorageRef.putFile(selectedImage!);
-    var downloadUrl= await (await task).ref.getDownloadURL();
+      final UploadTask task = firebaseStorageRef.putFile(selectedImage!);
+      var downloadUrl = await (await task).ref.getDownloadURL();
 
-    Map<String, dynamic> addProduct={
-      "Nama": namecontroller.text,
-      "Gambar": downloadUrl,
-
-    };
-    await DatabaseMethods().addProduct(addProduct, value!).then((value) {
-      selectedImage=null;
-      namecontroller.text="";
-    // ignore: use_build_context_synchronously
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      backgroundColor: Colors.redAccent,
-      content: Text("Produk berhasil di upload!", style: TextStyle(fontSize: 20.0),)));
-  });
+      Map<String, dynamic> addProduct = {
+        "Nama": namecontroller.text,
+        "Gambar": downloadUrl,
+        "Harga": pricecontroller,
+        "Detail": detailcontroller,
+      };
+      await DatabaseMethods().addProduct(addProduct, value!).then((value) {
+        selectedImage = null;
+        namecontroller.text = "";
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            backgroundColor: AppColors.primary,
+            content: Text(
+              "Produk berhasil di upload!",
+              style: TextStyle(fontSize: 20.0),
+            )));
+      });
+    }
   }
-}
+
   String? value;
   final List<String> categoryitem = ['Alat Kesehatan', 'Vitamin', 'Herbal'];
 
@@ -64,114 +72,185 @@ uploadItem() async {
           style: AppWidget.semiboldTextFeildStyle(),
         ),
       ),
-      body: Container(
-        margin: const EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Upload foto Produk",
-              style: AppWidget.lightTextFieldStyle(),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Center(
-              child: GestureDetector(
-                onTap: getImage,
-                child: Container(
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 1.5),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: selectedImage != null
-                      ? Image.file(selectedImage!)
-                      : const Icon(Icons.camera_alt_outlined),
-                ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0, bottom: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Upload foto Produk",
+                style: AppWidget.lightTextFieldStyle(),
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Nama Produk",
-              style: AppWidget.lightTextFieldStyle(),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: const Color(0xFFececf8),
-                borderRadius: BorderRadius.circular(20),
+              const SizedBox(
+                height: 20.0,
               ),
-              child: TextField(
-                controller: namecontroller,
-                decoration: const InputDecoration(border: InputBorder.none),
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Produk Kategori",
-              style: AppWidget.lightTextFieldStyle(),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: const Color(0xFFececf8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  items: categoryitem
-                      .map(
-                        (item) => DropdownMenuItem(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: AppWidget.semiboldTextFeildStyle(),
+              selectedImage == null
+                  ? GestureDetector(
+                      onTap: () {
+                        getImage();
+                      },
+                      child: Center(
+                        child: GestureDetector(
+                          onTap: getImage,
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black, width: 1.5),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Icon(Icons.camera_alt_outlined),
                           ),
                         ),
-                      )
-                      .toList(),
-                  onChanged: (value) => setState(() {
-                    this.value = value;
-                  }),
-                  dropdownColor: Colors.white,
-                  hint: const Text("Pilih Kategori"),
-                  iconSize: 36,
-                  icon: const Icon(
-                    Icons.arrow_drop_down,
-                    color: Colors.black,
+                      ),
+                    )
+                  : Center(
+                    child: Material(
+                        elevation: 4.0,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 1.5),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                              child: Image.file(
+                            selectedImage!,
+                            fit: BoxFit.cover,
+                          )),
+                        ),
+                      ),
                   ),
-                  value: value,
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Nama Produk",
+                style: AppWidget.lightTextFieldStyle(),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextField(
+                  controller: namecontroller,
+                  decoration: const InputDecoration(border: InputBorder.none),
                 ),
               ),
-            ),
-            const SizedBox(height: 30.0),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  uploadItem();
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: AppColors.primary,
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Harga Produk",
+                style: AppWidget.lightTextFieldStyle(),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                child: const Text(
-                  "Tambah Produk",
-                  style: TextStyle(fontSize: 22.0),
+                child: TextField(
+                  controller: pricecontroller,
+                  decoration: const InputDecoration(border: InputBorder.none),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Detail Produk",
+                style: AppWidget.lightTextFieldStyle(),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextField(
+                  maxLines: 6,
+                  controller: detailcontroller,
+                  decoration: const InputDecoration(border: InputBorder.none),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Produk Kategori",
+                style: AppWidget.lightTextFieldStyle(),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    items: categoryitem
+                        .map(
+                          (item) => DropdownMenuItem(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: AppWidget.semiboldTextFeildStyle(),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) => setState(() {
+                      this.value = value;
+                    }),
+                    dropdownColor: Colors.white,
+                    hint: const Text("Pilih Kategori"),
+                    iconSize: 36,
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.black,
+                    ),
+                    value: value,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    uploadItem();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: AppColors.primary,
+                  ),
+                  child: const Text(
+                    "Tambah Produk",
+                    style: TextStyle(fontSize: 22.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
